392ee2b145335590e0d8a44358b4323106a9a216
ManasSin <maltisadan12@gmail.com>
2024-06-24 23:35:10 +0530

    :sparkles: ADD: SpeedProvider context for controlling animation speed
    adds a new context called SpeedProvider to control the animation speed of the maze pathfinding algorithm. The existing code in the App component is wrapped with the SpeedProvider component, allowing components to access the animation speed information.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { PathFinderProvider } from "./context/PathfinderContext";
+import { SpeedProvider } from "./context/SpeedContext";
 import { TileProvider } from "./context/TileContext";
 
 function App() {
   return (
     <PathFinderProvider>
       <TileProvider>
-        <div className="">
-          <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">
-            hello world!
-          </h1>
-        </div>
+        <SpeedProvider>
+          <div className="">
+            <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">
+              hello world!
+            </h1>
+          </div>
+        </SpeedProvider>
       </TileProvider>
     </PathFinderProvider>
   );
