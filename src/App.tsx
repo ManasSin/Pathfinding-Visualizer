24f6c0173336a1de5fdc52d1adc76150a19bcceb
ManasSin <maltisadan12@gmail.com>
2024-06-24 21:58:08 +0530

    :sparkles: ADD: Added context for Tile in the maze
    a new context called TileProvider to provide tile information to the components. It wraps the existing code in the App component with the TileProvider component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { PathFinderProvider } from "./context/PathfinderContext";
+import { TileProvider } from "./context/TileContext";
 
 function App() {
   return (
     <PathFinderProvider>
-      <div className="">
-        <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">
-          hello world!
-        </h1>
-      </div>
+      <TileProvider>
+        <div className="">
+          <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">
+            hello world!
+          </h1>
+        </div>
+      </TileProvider>
     </PathFinderProvider>
   );
 }
