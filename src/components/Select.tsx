0a41141616387d1982c5b54fba49e9e3ac344977
ManasSin <maltisadan12@gmail.com>
2024-07-01 17:18:58 +0530

    :sparkles: feat: Add Select component for dropdown selection
    The Select component is a reusable component that allows users to select options from a dropdown menu. It accepts props for the selected value, options, onChange event handler, label, and disabled state. This component improves the user experience by providing a consistent and intuitive way to select values in the application.

---
+++ src/components/Select.tsx
@@ -0,0 +1,32 @@
+import React, { ChangeEvent } from "react";
+
+type Props = {
+  value: string | number;
+  options: { value: string | number; name: string }[];
+  onChange: (value: ChangeEvent<HTMLSelectElement>) => void;
+  label: string;
+  isDisabled?: boolean;
+};
+
+const Select = ({ value, onChange, label, options, isDisabled }: Props) => {
+  return (
+    <div className="flex flex-col items-start gap-1">
+      <label className="text-sm text-gray-300 ml-1">{label}</label>
+      <select
+        value={value}
+        onChange={onChange}
+        id={label}
+        disabled={isDisabled}
+        className="bg-gray-700 cursor-pointer hover:bg-gray-800 transition ease-in active:border-0 p-2 min-w-[200px] sm:min-w-full"
+      >
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default Select;
