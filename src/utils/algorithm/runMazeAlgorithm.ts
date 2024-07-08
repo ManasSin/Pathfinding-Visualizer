eefa32c759fae1ae98b04624748b739583d87775
ManasSin <maltisadan12@gmail.com>
2024-07-08 13:53:31 +0530

    :sparkles: ADD:recursive division algorithm for maze generation
    This commit adds the implementation of the recursive division algorithm for maze generation. It includes the necessary functions and logic to generate a maze using recursive division. The algorithm divides the maze into smaller sections recursively, creating walls to form the maze structure. This addition enhances the maze generation capabilities of the application.

--- src/utils/algorithm/runMazeAlgorithm.ts
+++ src/utils/algorithm/runMazeAlgorithm.ts
@@ -1,4 +1,7 @@
 import { BinaryTree } from "../../lib/algorithm/maze/BinaryTree";
+import recursiveDivision from "../../lib/algorithm/maze/RecursiveDivision";
+import { MAX_COLS, MAX_ROWS, SPEEDS } from "../constants";
+import { constructBorders } from "../constructBorder";
 import { GridType, MazeType, SpeedType, TileType } from "../types";
 
 type RunMazeAlgoType = {
@@ -20,5 +23,22 @@ export const runMazeAlgorithm = async ({
 }: RunMazeAlgoType) => {
   if (maze === "BINARY_TREE") {
     await BinaryTree(grid, startTile, endTile, setIsDisabled, speed);
+  } else if (maze === "RECURSIVE_DIVISION") {
+    const currentSpeed = SPEEDS.find((s) => s.value === speed)!.value ?? 2;
+    await constructBorders(grid, startTile, endTile);
+    await recursiveDivision({
+      grid,
+      startTile,
+      endTile,
+      row: 1,
+      col: 1,
+      height: Math.floor((MAX_ROWS - 1) / 2),
+      width: Math.floor((MAX_COLS - 1) / 2),
+      setIsDisabled,
+      speed,
+    });
+    setTimeout(() => {
+      setIsDisabled(false);
+    }, 800 * currentSpeed);
   }
 };
