126076d2bbebbd1236be4b2729c5c01a99c42916
ManasSin <maltisadan12@gmail.com>
2024-07-12 03:35:33 +0530

    :sparkles: ADD: Add Button component for UI interaction

---
+++ src/components/Button.tsx
@@ -0,0 +1,50 @@
+import React, { MouseEventHandler } from "react";
+import { twMerge } from "tailwind-merge";
+
+type Props = {
+  textForButton: string;
+  handleResetGrid: MouseEventHandler<HTMLButtonElement>;
+  isDisabled?: boolean;
+  className?: string;
+  type?: "danger" | "button" | "small" | "outline";
+};
+
+const Button = ({
+  textForButton,
+  handleResetGrid,
+  isDisabled = false,
+  className = "",
+  type,
+}: Props) => {
+  return (
+    <button
+      onClick={handleResetGrid}
+      disabled={isDisabled}
+      className={twMerge(`
+                relative
+                rounded-lg
+                hover:opacity-80
+                transition
+                text-center
+                w-fit
+                px-4
+                ${
+                  type === "outline"
+                    ? "bg-white border-black border text-black py-1.5"
+                    : type === "danger"
+                    ? "border-red-500 bg-red-400 text-white font-semibold"
+                    : type === "small"
+                    ? "font-thin text-xs py-1 px-3"
+                    : type === "button"
+                    ? "bg-rose-500 border-red-500 text-white py-2 text-md font-semibold"
+                    : ""
+                }
+                ${className}
+                `)}
+    >
+      {textForButton}
+    </button>
+  );
+};
+
+export default Button;
